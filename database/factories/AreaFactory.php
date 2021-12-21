c1a3c9abe10984ff332cbe7700dac64ed96d70c3
ali.sayed987 <ali.sayed987@yahoo.com>
2021-12-21 17:22:10 +0200

    pushing latest changes of factories

---
+++ database/factories/AreaFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Ramsey\Uuid\Type\Integer;
+
+class AreaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->unique()->name(),
+            'city_id' => $this->faker->numberBetw
